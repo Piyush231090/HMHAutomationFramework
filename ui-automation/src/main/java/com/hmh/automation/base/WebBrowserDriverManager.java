0e66be9e52b0699fa9cda9ca49230daf1f10db7d
Piyush231090 <x22154779@student.ncirl.ie>
2025-02-11 21:57:39 +0000

    added logging support and changed some test cases

--- ui-automation/src/main/java/com/hmh/automation/base/WebBrowserDriverManager.java
+++ ui-automation/src/main/java/com/hmh/automation/base/WebBrowserDriverManager.java
@@ -2,15 +2,21 @@ package com.hmh.automation.base;
 
 import java.time.Duration;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
+
+
 public class WebBrowserDriverManager {
 	
+	private static final Logger logger = LogManager.getLogger(WebBrowserDriverManager.class);
 	protected WebDriver driver;
     public WebDriver setup(String browser) {
+    	logger.info("Initializing WebDriver...");
         if (browser.equalsIgnoreCase("chrome")) {
             driver = new ChromeDriver(); 
         } else if (browser.equalsIgnoreCase("firefox")) {
